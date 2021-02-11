9e97c8f760686574f2ee7731a82c57156fb1855a
Jonathan Getahun <kgetahun2@gmail.com>
2021-02-10 22:08:17 -0500

    rough draft

--- task.js
+++ task.js
@@ -1,19 +1,32 @@
 const http = require('http');
-const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 4000;
 
 
-let storage = [];
+let storage = {};
 
 const server = http.createServer((req,res) => {
     const { url }=req;
     current_url = new URL(`http://${hostname}:${port}${url}`);
     const keyValuePairs = current_url.searchParams.entries();
-    for (const [key,value] of keyValuePairs){
+
+
+    if(url.substring(1,4) === "set"){
+        for (const [key,value] of keyValuePairs){
+            storage[key] = value
+        }
+    } else if(url.substring(1,4) === "get"){
         
+        for(const [key,somekey] of keyValuePairs){ 
+            res.write(`${somekey}:${storage[somekey]}\n`)
+        }
+    } else {
+        res.statusCode = 404
+        res.end("please use a set or get request")
     }
+
+    console.log(storage)
     res.end(url)
 })
 
